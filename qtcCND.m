d7bd655eccdb174ac29abc41126c2405f040a773
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-01-07 20:36:46 +0000

    Fixed the transition probabilities by including a lot of if's Very ugly but works. Produces the wanted 1088 transitions.

--- qtcCND.m
+++ qtcCND.m
@@ -1,7 +1,7 @@
 function hmm = qtcCND;
-validtr=zeros(81,81);
+% validtr=zeros(81,81);
 
-[c,from]=qtc2case([-1 -1 -1 -1]);
+% [c,from]=qtc2case([-1 -1 -1 -1]);
 
 qtc=[];
 for i1=1:3
@@ -14,10 +14,111 @@ for i1=1:3
 	end;
 end;
 
+validqtc=[];
+
 d=zeros(83,83);
 for i1=1:size(qtc,1)
 	for i2=1:size(qtc,1)
 		d(i1+1,i2+1)=max(abs(qtc(i1,:)-qtc(i2,:)));
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % e.g. -0 to 0-        
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,1:2)))==1 & sum(abs(qtc(i2,1:2)))==1
+                if max(abs(qtc(i1,1:2)-qtc(i2,1:2))) > 0 & sum(qtc(i1,1:2)-qtc(i2,1:2))==0
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,3:4)))==1 & sum(abs(qtc(i2,3:4)))==1
+                if max(abs(qtc(i1,3:4)-qtc(i2,3:4))) > 0 & sum(qtc(i1,3:4)-qtc(i2,3:4))==0
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % e.g. -0 to 0+
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,1:2)))==1 & sum(abs(qtc(i2,1:2)))==1
+                if max(abs(qtc(i1,1:2)-qtc(i2,1:2))) > 0 & abs(sum(qtc(i1,1:2)-qtc(i2,1:2)))==2
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,3:4)))==1 & sum(abs(qtc(i2,3:4)))==1
+                if max(abs(qtc(i1,3:4)-qtc(i2,3:4))) > 0 & abs(sum(qtc(i1,3:4)-qtc(i2,3:4)))==2
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % e.g. ++0+ to 0+++
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[1,3])))==1 & sum(abs(qtc(i2,[1,3])))==1
+                if max(abs(qtc(i1,[1,3])-qtc(i2,[1,3]))) > 0 & sum(qtc(i1,[1,3])-qtc(i2,[1,3]))==0
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[2,4])))==1 & sum(abs(qtc(i2,[2,4])))==1
+                if max(abs(qtc(i1,[2,4])-qtc(i2,[2,4]))) > 0 & sum(qtc(i1,[2,4])-qtc(i2,[2,4]))==0
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % e.g. ++0+ to 0+-+
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[1,3])))==1 & sum(abs(qtc(i2,[1,3])))==1
+                if max(abs(qtc(i1,[1,3])-qtc(i2,[1,3]))) > 0 & abs(sum(qtc(i1,[1,3])-qtc(i2,[1,3])))==2
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[2,4])))==1 & sum(abs(qtc(i2,[2,4])))==1
+                if max(abs(qtc(i1,[2,4])-qtc(i2,[2,4]))) > 0 & abs(sum(qtc(i1,[2,4])-qtc(i2,[2,4])))==2
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % e.g. 00-0 to 0-00
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[2,3])))==1 & sum(abs(qtc(i2,[2,3])))==1
+                if max(abs(qtc(i1,[2,3])-qtc(i2,[2,3]))) > 0 & sum(qtc(i1,[2,3])-qtc(i2,[2,3]))==0
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[1,4])))==1 & sum(abs(qtc(i2,[1,4])))==1
+                if max(abs(qtc(i1,[1,4])-qtc(i2,[1,4]))) > 0 & sum(qtc(i1,[1,4])-qtc(i2,[1,4]))==0
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % e.g. 00+0 to 0-00
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[2,3])))==1 & sum(abs(qtc(i2,[2,3])))==1
+                if max(abs(qtc(i1,[2,3])-qtc(i2,[2,3]))) > 0 & abs(sum(qtc(i1,[2,3])-qtc(i2,[2,3])))==2
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        if d(i1+1,i2+1)==1
+            if sum(abs(qtc(i1,[1,4])))==1 & sum(abs(qtc(i2,[1,4])))==1
+                if max(abs(qtc(i1,[1,4])-qtc(i2,[1,4]))) > 0 & abs(sum(qtc(i1,[1,4])-qtc(i2,[1,4])))==2
+                    d(i1+1,i2+1)=2;
+                end
+            end
+        end
+        if d(i1+1,i2+1)==1
+            validqtc(end+1,:)=[qtc(i1,:), 9 ,qtc(i2,:)];
+        end
 	end;
 end;
 validtr=double(d==1);
@@ -41,4 +142,5 @@ emmissions=emmissions./repmat(sum(emmissions,2),1,size(emmissions,2));
 
 hmm.t=t;
 hmm.e=emmissions;
+hmm.q = validqtc;
 
