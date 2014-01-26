07dc3467a6549f5b25c733d48ff4787d1f117447
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-01-26 22:44:28 +0000

    Starting to adapt qtcCrossValidation. qtcSeqDecode still needs to be altered for that to work.

--- qtcCrossValidation.m
+++ qtcCrossValidation.m
@@ -1,13 +1,14 @@
-function [ p ] = qtcCrossValidation( data_set, control, mode, KorP )
+function [ p ] = qtcCrossValidation( data_set, control, mode, type, KorP )
 %QTCCROSSVALIDATION
 %   Uses cross validation to evaluate the difference between the given
 %   DATA_SET and the CONTROL. Returns classification probabilities.
 %
-%   P = QTCCROSSVALIDATION(DATA_SET, CONTROL, MODE, KORP) generates HMM
+%   P = QTCCROSSVALIDATION(DATA_SET, CONTROL, MODE, TYPE, KORP) generates HMM
 %   representations of DATA_SET and CONTROL and returns classification
 %   rates for DATA_SET using CONTROL as a control. DATA_SET and CONTROL are
-%   column matrixes. Set MODE to 'Kfold' or 'HoldOut'. See below for KORP
-%   description.
+%   column matrixes. Set MODE to 'Kfold' or 'HoldOut'. TYPE has to be 
+%   'qtcb', 'qtcc,' or 'combined' dependign on what you want to do.See below 
+%   for KORP description.
 %
 %   Kfold: Uses KORP for number of iterations. Default KORP = 5. Devides
 %   DATA_SET into KORP number of sets and uses each as a test set in a
@@ -16,16 +17,18 @@ function [ p ] = qtcCrossValidation( data_set, control, mode, KorP )
 %   HoldOut: Uses KORP as the percentage of DATA_SET for the test set.
 %   Default KORP = .5. Only runs once.
 
-if size(data_set,2) == 2
+if strcmp(type,'qtcb')
     cnd = qtcCND('type','qtcb');
-elseif size(data_set,2) == 4
+elseif strcmp(type,'qtcc')
     cnd = qtcCND('type','qtcc');
+elseif strcmp(type,'combined')
+    cnd = qtcCND('type','combined');
 end
 
 if strcmp(mode,'Kfold')
     disp('---------------------------------------------------------------')
     disp('K-fold crossvalidation')
-    if nargin > 3
+    if nargin > 4
         indices = crossvalind(mode, 1:length(data_set), KorP);
         disp(['k = ',num2str(KorP)])
     else
@@ -33,7 +36,7 @@ if strcmp(mode,'Kfold')
         disp('k = 5')
     end
     disp('---------------------------------------------------------------')
-    hmm_control = qtcTrainHmm(cnd, control);
+    hmm_control = qtcTrainHmm(cnd, control, type);
     p = [];
     for i=1:max(indices)
         disp(['Run: ',num2str(i)])
@@ -42,7 +45,7 @@ if strcmp(mode,'Kfold')
         training_set = data_set(indices ~= i);
         
         % data
-        hmm_test=qtcTrainHmm(cnd, training_set);
+        hmm_test=qtcTrainHmm(cnd, training_set, type);
         res_test=qtcSeqDecode(hmm_test, test_set);
         
         % control
@@ -55,7 +58,7 @@ if strcmp(mode,'Kfold')
 elseif strcmp(mode,'HoldOut')
     disp('---------------------------------------------------------------')
     disp('Hold-Out crossvalidation')
-    if nargin > 3
+    if nargin > 4
         [Train, Test] = crossvalind('HoldOut', length(data_set), KorP);
         disp(['p = ',num2str(KorP)])
     else
@@ -67,9 +70,9 @@ elseif strcmp(mode,'HoldOut')
     training_set = data_set(Train);
 
     % data
-    hmm_control = qtcTrainHmm(cnd, control);
+    hmm_control = qtcTrainHmm(cnd, control, type);
     p = [];
-    hmm_test=qtcTrainHmm(cnd, training_set);
+    hmm_test=qtcTrainHmm(cnd, training_set, type);
     res_test=qtcSeqDecode(hmm_test, test_set);
 
     % control
